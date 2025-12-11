7eea85f879f78061d1b7f4e8cbdaf58c545bc6b6
Nithin <nithin.shsr@gmail.com>
2025-12-11 22:18:37 +0530

    Minor changes carried out in the documentation part of Event Loop - Server example

--- Event Loop Examples/server.js
+++ Event Loop Examples/server.js
@@ -5,6 +5,6 @@ const server = http.createServer((req, res) => {
     res.end('Hello World')
 })
 const number = 8000;
-server.listen(number,()=>{
+server.listen(number,()=>{ //Here .listen is asynchronous so it gets offloaded by event loop
     console.log(`Server listening on port ${number}`)
 })
