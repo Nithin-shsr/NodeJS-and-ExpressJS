188ed23f234667d6f93a4c8bc540b8c11664063b
Nithin <nithin.shsr@gmail.com>
2025-12-16 15:26:47 +0530

    Created a server that resolves all the issues mentioned earlier

--- ExpressJS Module/My_First_Server.js
+++ ExpressJS Module/My_First_Server.js
@@ -1,4 +1,5 @@
-const http = require('http');
+/**Basic Server coded below*/
+/*const http = require('http');
 
 const server = http.createServer((req,res)=>{
     console.log(`User opened a server at ${port}`);
@@ -7,8 +8,47 @@ const server = http.createServer((req,res)=>{
 
 const port = 3000;
 
-server.listen(port);
+server.listen(port);*/
 
+
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+    // console.log(req.method);
+    // console.log(req.url);
+
+    const url = req.url;
+
+    if(url === '/')
+    {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<h1>Welcome to our server!</h1>');
+        res.end();
+    }
+
+    //For the about page
+    else if(url === '/about')
+    {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<h1>You are in the about page!</h1>');
+        res.end();
+    }
+
+
+    //404
+    else
+    {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.write('<h1>Oops! Page not found..</h1>');
+        res.end();
+    }
+
+
+})
+
+const port = 3000;
+
+server.listen(port);
 
 
 /**
