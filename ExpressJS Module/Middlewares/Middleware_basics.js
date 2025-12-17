95420fbc9121d40e381879f36b760d92525a9cb1
Nithin <nithin.shsr@gmail.com>
2025-12-17 14:40:47 +0530

    Implememted middlewares using .use() method

--- ExpressJS Module/Middlewares/Middleware_basics.js
+++ ExpressJS Module/Middlewares/Middleware_basics.js
@@ -2,13 +2,29 @@ const express = require('express');
 const app = express();
 const logger = require('./Logger middleware');
 
+app.use(logger);
 
-app.get('/',logger,(req,res)=>{
+/**
+ * Instead of using logger middleware in each an every routes that we are creating we can declare them only once
+ * and can be used for all
+ *
+ * The above can be implemented using  .USE() method
+ */
+
+/*
+Note ;
+    1. Order matters in Middleware
+        If app.use(logger) is placed after app.get('/', (req, res)),
+        then the logger middleware will only apply to routes that are defined below it.
+        Routes defined above will not use the logger.
+        */
+
+app.get('/',(req,res)=>{
 
     res.send('This is our home page');
 })
 
-app.get('/about',logger,(req,res)=>{
+app.get('/about',(req,res)=>{
 
     res.send('This is our about page');
 })
