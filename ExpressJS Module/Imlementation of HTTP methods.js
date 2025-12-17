bed5b59764ea603f073fb44d5688b1ffa5b18200
Nithin <nithin.shsr@gmail.com>
2025-12-17 18:33:45 +0530

    Implemented POST method using sample websites

--- ExpressJS Module/Imlementation of HTTP methods.js
+++ ExpressJS Module/Imlementation of HTTP methods.js
@@ -2,9 +2,37 @@ const express = require('express');
 const app = express();
 const {people} = require('./data_for_json');
 
+//Static assets
+
+app.use(express.static('./Methods-example'))
+//Parse form data
+app.use(express.urlencoded({extended : false}))
+//Parse Json data - To handle the json data 
+app.use(express.json())
+
 app.get('/api/people', (req, res) => {
-    res.set('Cache-Control', 'no-store');
-    res.status(200).json({success:true,data:people});
+    res.status(200).json({success:true,data:people})
+})
+
+app.post('/api/people', (req, res) => {
+    const {name} = req.body;
+    if(!name){
+        return res.status(400).json({success:false,msg:'Please provide name value'})
+    }
+    else{
+    return res.status(201).json({success:true,person:name})
+    }
+})
+
+app.post('/login',(req,res)=>{
+    const {name} = req.body;
+    if(name){
+        res.status(200).send(`Welcome to the website ${name}`)
+    }
+    else{
+        res.status(404).send("Please provide credentials")
+    }
+
 })
 
 app.listen(3000, () => {
